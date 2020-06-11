cb1621166ab067f00df67dacc0adee7d13fc77f4
Dariaki <dashakirich@gmail.com>
2020-06-11 19:30:18 +0300

    task8

--- src/index.js
+++ src/index.js
@@ -41,6 +41,32 @@ const formatPresident = (name, lastName, index) => {
   return `${name}, ${lastName}, ${index}th`;
 };
 
+const serverData = "[{\"id\":\"666958530825467\",\"title\":\"Friday open-air party w/ Lucarelli, Mihas and Yarik JR | Hide\",\"place\":\"Hide\",\"date\":\"2020-06-12T20:00:00.000Z\"},{\"id\":\"786185895252176\",\"title\":\"Захист скверу імені Чкалова\",\"place\":\"Сквер Им. Чкалова\",\"date\":\"2020-06-10T09:00:00.000Z\"},{\"id\":\"623921328209118\",\"title\":\"Живая музыка на летней террасе\",\"place\":\"От Заката до Рассвета\",\"date\":\"2020-06-14T17:00:00.000Z\"},{\"id\":\"909559356190295\",\"title\":\"Amer (2009)\",\"place\":\"Кіноклуб Кіноха\",\"date\":\"2020-06-13T15:00:00.000Z\"},{\"id\":\"589272605321022\",\"title\":\"В парк Межигорье на теплоходе\",\"place\":\"Причал №6, Почтовая пл.\",\"date\":\"2020-06-13T07:45:00.000Z\"}]";
+const parsedServerData = JSON.parse(serverData);
+
+const sortData = (data) => {
+  return [...data].sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+};
+
+const formatDate = (d) => {
+
+  const fDate = d.toLocaleDateString();
+  const fTime = d.toLocaleTimeString().split(' ')[0];
+
+  let fDayTime;
+  if (d.getHours() > 3 && d.getHours() < 12) {
+    fDayTime = 'Утро';
+  } else if (d.getHours() < 18) {
+    fDayTime = 'День';
+  } else if (d.getHours() < 23) {
+    fDayTime = 'Вечер';
+  } else {
+    fDayTime = 'Ночь';
+  }
+  return `${fDayTime}, ${fDate}, ${fTime}`;
+};
+
+
 ReactDOM.render(
   <div>
     <ul>
@@ -62,6 +88,25 @@ ReactDOM.render(
           {formatPresident(president.firstName, president.lastName, president.presidentIndex)}
         </li>
       ) : null)}
+    </ul>
+    <ul>
+      {sortData(parsedServerData).map(item => {
+
+          const date = new Date(item.date);
+
+          const isOutDated = () => {
+            return date.getTime() < new Date().getTime() ? {color:'rgba(0,0,0,.5)'} : {}
+          };
+
+          return (
+            <li key={item.id} style={isOutDated()}>
+              <a href='#' target="_blank"  style={isOutDated()}>{item.title}</a>
+              <p>{`${formatDate(date)}`}</p>
+              <p>{item.place}</p>
+            </li>
+          )
+        }
+      )}
     </ul>
   </div>,
   document.getElementById('root')
